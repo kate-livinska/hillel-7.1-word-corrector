ce3ed54953c09f23c9873fa8647cb8a0152838a0
Kateryna Livinska <kateryna.livinska@gmail.com>
2024-10-15 20:46:01 +0200

    Fix Main to provide expected output

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,4 +1,14 @@
 package app;
 
 public class Main {
+
+    public static void main(String[] args) {
+        getOutput(new Corrector().handleData(
+                new DataProvider().getData())
+        );
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
+    }
 }
